018f30236f0ad2aafd1af822fa96a223066051f2
judah-levi <kolamiti92@gmail.com>
2020-09-09 14:32:12 +0300

    changes

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -5,7 +5,7 @@ import Axios from 'react-native-axios'
 
 // Initialize Firebase
 let config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
