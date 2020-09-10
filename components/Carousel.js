1c3b9812611bee39001c45fc7cd2ccaa0f738985
judah-levi <kolamiti92@gmail.com>
2020-09-10 12:11:52 +0300

    implemented firestore query

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,28 +1,40 @@
 import Carousel from 'react-native-snap-carousel';
-import {React, Component } from 'react';
+import React from 'react';
+import firestore from '@react-native-firebase/firestore'
+import { TouchableOpacity, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 
-export default class MyCarousel extends Component {
+export default function MyCarousel() {
+    
+    const db = firestore();
+    const news = db.collection('news-updates');
 
-    _renderItem = ({item, index}) => {
-        return (
-            <View style={styles.slide}>
-                <Text style={styles.title}>{ item.title }</Text>
-            </View>
-        );
-    }
+    const getNews = ()  => {
+        news.get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc)  {
+                console.log(doc.data().title);
+            });
+        })
+    };
 
-    render () {
-        return (
-            <Carousel
-              layout={'stack'} 
-              layoutCardOffset={`18`} 
-              ref={(c) => { this._carousel = c; }}
-              data={this.state.entries}
-              renderItem={this._renderItem}
-              sliderWidth={sliderWidth}
-              itemWidth={itemWidth}
-            />
-        );
-    }
+    return (
+        // <Carousel
+        //   layout={'stack'} 
+        //   layoutCardOffset={`18`} 
+        //   ref={(c) => {_carousel = c; }}
+        // //   data={this.state.entries}
+        //   renderItem={_renderItem}
+        //   sliderWidth={sliderWidth}
+        //   itemWidth={itemWidth}
+        // 
+        <View>
+        <TouchableOpacity
+            onPress={getNews}
+        >
+            <Text>Console Log</Text>
+        </TouchableOpacity>
+        </View>
+    );
 }
--- components/MainPage.js
+++ components/MainPage.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from "react";
 import { Avatar, Title, Text } from 'react-native-paper';
-import MyCarousel from './Carousel';
 import NavBar from './NavBar'
 import auth from '@react-native-firebase/auth'
 import { useNavigation } from "@react-navigation/native";
@@ -9,30 +8,19 @@ import {
     View,
     TouchableOpacity,
   } from "react-native";
+import MyCarousel from "./Carousel";
 
   
-  export default class MainPage extends React.Component {
-   state = { 
-       currentUser: null,
-    }
+  export default function MainPage() {
 
-   componentDidMount()  {
-       const { currentUser } = auth()
-       this.setState({currentUser})
-   }
-
-
-   render()  {
-    const {currentUser} = this.state
 
     return(
         <View style={styles.mainWrapper}>
             <NavBar/>
-            <Text>this is the main page</Text>
-            {/* <MyCarousel/> */}
+            {/* <Text>this is the main page</Text> */}
+            <MyCarousel />
         </View>
     );
-    }
 }
 
 
