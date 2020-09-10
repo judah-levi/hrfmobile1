2313b5ca24154747cd82ebc178a5a598f1200a8d
judah-levi <kolamiti92@gmail.com>
2020-09-10 18:55:28 +0300

    added picture handling

--- components/adminComponents/FormCarousel.js
+++ components/adminComponents/FormCarousel.js
@@ -1,58 +1,121 @@
-import React, {useState} from 'react'
-import {StyleSheet, TouchableOpacity, Text, Image, View} from "react-native";
+import React, {setState} from 'react'
+import {StyleSheet, TouchableOpacity, Text, View, Image} from "react-native";
 import AdminNavBar from './adminNavBar';
 import { TextInput } from 'react-native-paper';
 import ImagePicker from 'react-native-image-picker'
+import firestore from '@react-native-firebase/firestore'
 
-function FormCarousel(){
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
 
-    const handleLibraryPhoto = () => {
-        const options = {};
-        ImagePicker.launchImageLibrary(options, response => {
-            console.log('response', response)
-        })
-    }
-    const handleShootPhoto = () => {
-        const options = {};
-        ImagePicker.launchCamera(options, response => {
-        })
+class FormCarousel extends React.Component {
+    constructor(props)  {
+        super(props)
+        this.state = {
+            title: '',
+            content: '',
+            image: null
+        }
     }
 
+    uploadPost = (post) => {
+        const uploadData = {
+            title: post.title,
+            content: post.content,
+            image: post.image
+        }
+        return firestore().collection('news-updates').doc().set(uploadData)
+    }
+
+    handleLibraryPhoto = () => {
+        const options = {
+            storageOptions:  {
+                path: 'images',
+            }
+          }
+          ImagePicker.launchImageLibrary(options, (response) => {
+            if (response.didCancel) {
+              console.log('User cancelled image picker')
+            } else if (response.error) {
+              console.log('ImagePicker Error: ', response.error)
+            } else if (response.customButton) {
+              console.log('User tapped custom button: ', response.customButton)
+            } else {
+              const source = {uri: response.uri};
+              this.setState({image: source});
+              console.log(this.state.image)
+            }
+          })
+    }
+
+    handleShootPhoto = () => { 
+        const options = {            
+            path: 'images'
+        };
+        ImagePicker.launchCamera(options, (response) => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker')
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error)
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton)
+            } else {
+                const source = { uri: response.uri };
+                this.setState({image: source})
+                console.log(this.state.image)
+            }
+        })
+    }
+
+    handleSubmit = async () =>  {
+        const post = {
+          image: this.state.image,
+          title: this.state.title,
+          content: this.state.content
+        }
+        this.uploadPost(post)
+        
+        // setImage({image: null})
+        // setTitle({title: ''})
+        // setContent({content: ''})
+    }
+    
+
+    render()  {
     return(
         <View>
             <AdminNavBar />
             <Text style={styles.hOneCarousel}>Push company-wide news updates:</Text>
             <View style={styles.carouselWrapper}>
-            <TextInput
-                style={styles.carouselInput}
-                mode='outlined'
-                placeholder="Title"
-                value={title}
-                onChangeText={title => setTitle(title)}
-            />
-            <TextInput
-                style={styles.carouselInput}
-                mode='outlined'
-                placeholder="Content"
-                value={content}
-                onChangeText={content => setContent(content)}
-            />
-            <View style={styles.photo}>
-                <TouchableOpacity onPress={handleLibraryPhoto}>
-                    <Text style={styles.left}>Upload a picture</Text>
+                <TextInput
+                    style={styles.carouselInput}
+                    mode='outlined'
+                    placeholder="Title"
+                    onChangeText={title => this.setState({title: title})}
+                />
+                <TextInput
+                    style={styles.carouselInput}
+                    mode='outlined'
+                    placeholder="Content"
+                    onChangeText={content => this.setState({content: content})}
+                />
+                <View>
+                    <TouchableOpacity 
+                        onPress={this.handleLibraryPhoto}
+                        >
+                        <Text style={styles.left}>Upload a picture</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        onPress={this.handleShootPhoto}
+                    >
+                        <Text style={styles.right}>Take a picture</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={styles.btnCarousel} onPress={this.handleSubmit}>
+                    <Text style={styles.btnTextCarousel}>Submit</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={handleShootPhoto}>
-                    <Text style={styles.right}>Take a picture</Text>
-                </TouchableOpacity>
-            </View>
-            <TouchableOpacity style={styles.btnCarousel}>
-                <Text style={styles.btnTextCarousel}>Submit</Text>
-            </TouchableOpacity>
             </View>
         </View>
     )
+    }
 }
 
 const styles = StyleSheet.create({
