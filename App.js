35ca1e9c3908e46e198a19b6b8ac57421c395c01
nico <nicovg_95@hotmail.com>
2020-10-10 21:22:15 +0300

    asd

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {useState, useEffect} from 'react';
 import EnterPage from './components/EnterPage';
-import MainPage from "./components/MainPage";
-import SignupPage from "./components/SignupPage";
-import LoginPage from "./components/LoginPage";
-import PersonalPage from './components/PersonalPage'
-import BusinessPage from './components/BusinessPage'
-import TimeOff from './components/forms/TimeOff'
-import Meetings from './components/forms/Meetings'
+import MainPage from './components/MainPage';
+import SignupPage from './components/SignupPage';
+import LoginPage from './components/LoginPage';
+import PersonalPage from './components/PersonalPage';
+import BusinessPage from './components/BusinessPage';
+import TimeOff from './components/forms/TimeOff';
+import Meetings from './components/forms/Meetings';
 import SickDay from './components/forms/SickDay';
 import CovidPage from './components/COVIDPage';
 import CovidFormEn from './components/forms/COVID-19-en';
@@ -18,80 +18,77 @@ import EquipmentFailure from './components/forms/EquipmentFailure';
 import FacilitiesIssues from './components/forms/FacilitiesIssues';
 import MaterialsNeeded from './components/forms/MaterialsNeeded';
 import Suggestion from './components/forms/Suggestion';
-import AdminPage from './components/adminComponents/AdminPage'
+import AdminPage from './components/adminComponents/AdminPage';
 import FormCarousel from './components/adminComponents/FormCarousel';
-import * as RNLocalize from 'react-native-localize'
-import i18n from 'i18n-js'
-import memoize from 'lodash.memoize'
+import * as RNLocalize from 'react-native-localize';
+import i18n from 'i18n-js';
+import memoize from 'lodash.memoize';
 
 const Stack = createStackNavigator();
 
 const translationGetters = {
   en: () => require('./translations/en.json'),
-  es: () => require('./translations/es.json')
-}
+  es: () => require('./translations/es.json'),
+};
 
 const translate = memoize(
   (key, config) => i18n.t(key, config),
-  (key, config) => (config ? key + JSON.stringify(config) : key)
-)
+  (key, config) => (config ? key + JSON.stringify(config) : key),
+);
 
 const setI18nConfig = () => {
-  const fallback = { languageTag: 'en' }
-  const { languageTag } =
-  RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) ||
-  fallback
+  const fallback = {languageTag: 'en'};
+  const {languageTag} =
+    RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) ||
+    fallback;
 
-  translate.cache.clear()
+  translate.cache.clear();
 
-  i18n.translations = { [languageTag]: translationGetters[languageTag]() }
-  i18n.locale = languageTag
-}
-
+  i18n.translations = {[languageTag]: translationGetters[languageTag]()};
+  i18n.locale = languageTag;
+};
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
-    setI18nConfig()
-    this.state =  {
-      isEnter: true
-    }
+    super(props);
+    // setI18nConfig();
+    this.state = {
+      isEnter: true,
+    };
   }
-  // state  
   // const [isEnter, setIsEnter] = useState(true);
 
-  componentDidMount() {
-    RNLocalize.addEventListener('change', this.handleLocalizationChange)
-  }
+  // componentDidMount() {
+  //   RNLocalize.addEventListener('change', this.handleLocalizationChange);
+  // }
 
-  componentWillUnmount() {
-    RNLocalize.removeEventListener('change', this.handleLocalizationChange)
-  }
+  // componentWillUnmount() {
+  //   RNLocalize.removeEventListener('change', this.handleLocalizationChange);
+  // }
 
   handleLocalizationChange = () => {
-    this.state.setI18nConfig
-    .then(() => forceUpdate())
-    .catch(error => {
-        console.error(error)
-    })
-  }
-
+    setI18nConfig()
+      .then(() => forceUpdate())
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   // useEffect(() =>  {
   //     RNLocalize.addEventListener('change', handleLocalizationChange)
-  // }, []);  
-  
+  // }, []);
+
   // useEffect( () =>  {
   //   setTimeout( () =>  {
   //     setIsEnter(false)
   //   }, 2000)
   // }, []);
 
-  render()  {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false,}}>
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
           {/* <Stack.Screen name="Login" component={isEnter ? EnterPage : LoginPage} /> */}
-          <Stack.Screen name="MainPage" component={MainPage} />  
+          <Stack.Screen name="MainPage" component={MainPage} />
           <Stack.Screen name="Signup" component={SignupPage} />
           <Stack.Screen name="PersonalPage" component={PersonalPage} />
           <Stack.Screen name="BusinessPage" component={BusinessPage} />
@@ -101,13 +98,23 @@ export default class App extends React.Component {
           <Stack.Screen name="CovidPage" component={CovidPage} />
           <Stack.Screen name="CovidFormEn" component={CovidFormEn} />
           <Stack.Screen name="CovidFormEs" component={CovidFormEs} />
-          <Stack.Screen name="EquipmentFailuresForm" component={EquipmentFailure} />
-          <Stack.Screen name="FacilitiesIssuesForm" component={FacilitiesIssues} />
-          <Stack.Screen name="MaterialsNeededForm" component={MaterialsNeeded} />
+          <Stack.Screen
+            name="EquipmentFailuresForm"
+            component={EquipmentFailure}
+          />
+          <Stack.Screen
+            name="FacilitiesIssuesForm"
+            component={FacilitiesIssues}
+          />
+          <Stack.Screen
+            name="MaterialsNeededForm"
+            component={MaterialsNeeded}
+          />
           <Stack.Screen name="SuggestionForm" component={Suggestion} />
           <Stack.Screen name="FormCarousel" component={FormCarousel} />
           <Stack.Screen name="AdminPage" component={AdminPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )};
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
